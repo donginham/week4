6c00bd0f2947aa430dded74b9e60334a816e3293
HDI <hdihdi1869@gmail.com>
2025-07-11 11:32:43 +0900

    MovieTableViewController 파일 생성

---
+++ week4/MovieTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieTableViewController.swift
+//  week4
+//
+//  Created by HDI on 7/11/25.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
